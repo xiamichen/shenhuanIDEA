bd22e6ce4ca1875f4f8c53b2f6c71073975ade76
xiamichen <1757789374@qq.com>
2017-06-10 10:35:14 +0800

    Initial commit

---
+++ src/main/java/com/chinaoct/ShenhuanApplication.java
@@ -0,0 +1,12 @@
+package com.chinaoct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShenhuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShenhuanApplication.class, args);
+	}
+}
